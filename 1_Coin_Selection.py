e9f811192c2337e0ee6bac9079e49364203b1104
Muzmmil548 <muzammilfayyaz1234@gmail.com>
2025-05-16 16:13:10 +0500

    Delete 1_Coin_Selection.py

--- 1_Coin_Selection.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-st.header("سکہ منتخب کریں")
-symbol = st.text_input("سکہ منتخب کریں (مثال: BTCUSDT)", value="BTCUSDT").upper()
-st.session_state["symbol"] = symbol
-st.success(f"منتخب سکہ: {symbol}")
